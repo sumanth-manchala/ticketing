1170ec5808890578df86311946653b5361fb31a0
sumanth-manchala <manchala.msr@gmail.com>
2020-11-05 21:47:33 +0530

    Add startup log for payments service

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -51,7 +51,7 @@ const start = async () => {
   }
 
   app.listen(3000, () => {
-    console.log("Server running on port 3000!!!!!");
+    console.log("Payments service running on port 3000!!!!!");
   });
 };
 
